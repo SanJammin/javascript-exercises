bcb0fecd6e0408e1dc9464cdf0448f9e685543a9
SanJammin <freddie.farr893@gmail.com>
2024-09-04 19:04:45 +0100

    Complete removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter((items) => !args.includes(items));
 };
+
+console.log(removeFromArray([1,2,3,4],3,4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
