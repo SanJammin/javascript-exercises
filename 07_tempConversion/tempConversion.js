4d9e5f035ad5ef818d3f381e1f036c2dbc14ed63
SanJammin <freddie.farr893@gmail.com>
2024-09-09 18:09:13 +0100

    Complete challenge

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let newTempCelsius = ((fahrenheitTemp-32)*5)/9;
+
+  return Math.round(newTempCelsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let newTempFahrenheit = (celsiusTemp*1.8)+32;
+
+  return Math.round(newTempFahrenheit *10)/10;
 };
 
 // Do not edit below this line
